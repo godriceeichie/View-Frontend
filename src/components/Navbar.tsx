a2337ce0077819cb13f10e34f509f36dce2d6485
godriceeichie <emmaeichie@gmail.com>
2024-02-06 14:22:55 +0100

    Almost done with the frontend

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,10 +4,10 @@ import { FiMenu } from "react-icons/fi";
 
 const Navbar = () => {
   return (
-    <header className="bg-primary-color px-6 md:px-10 py-3 flex items-center justify-between">
+    <header className="sticky top-0 z-10 bg-primary-color px-6 md:px-10 py-3 flex items-center justify-between">
       <div className="flex items-center gap-28">
         <Logo />
-        <ul className="hidden md:flex gap-6 text-white text-sm ">
+        <ul className="hidden lg:flex gap-6 text-white text-sm ">
           <li>
             <Link to={"#"} className="">
               Use cases
@@ -27,7 +27,7 @@ const Navbar = () => {
       <div className="flex items-center gap-5">
         <Link
           to={"/auth/login"}
-          className="bg-[#E7E8FF] text-white bg-opacity-15 px-[24px] py-[6px] rounded-md text-sm"
+          className="bg-[#E7E8FF] text-white bg-opacity-15 px-[24px] py-[8px] rounded-md text-sm"
         >
           Login
         </Link>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,18 +2,19 @@ import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
-    <section className="bg-primary-color h-[87vh] text-white">
+    <section className="bg-primary-color h-[87vh] md:h-[75vh] text-white">
       <div className="h-full">
-        <div className="flex flex-col items-center justify-center h-full">
+        <div className="flex flex-col items-center justify-center h-full gap-10 ">
           <div className="flex flex-col items-center text-center gap-5">
-            <h1 className="text-6xl w-3/4">The best performance management system</h1>
-            <p className="w-1/3">
+            <h1 className="text-5xl md:text-6xl px-5 w-full md:w-3/4">The best performance management system</h1>
+            <p className="md:w-1/3 text-white text-opacity-75 px-10 md:px-0">
               We assist businesses scale positive, and increase employee
               engagement, output, and retention.
             </p>
           </div>
-          <div>
-            <Link to={"/auth/get-started"} >Get Started</Link>
+          <div className="flex items-center gap-3">
+            <Link to={"/auth/get-started"} className="bg-white text-primary-color text-sm md:text-base px-[1.25em] py-[0.75em] rounded-lg">Get Started</Link>
+            <Link to={"/"} className={"bg-[#E7E8FF] bg-opacity-15 text-sm md:text-base px-[1.25em] py-[0.75em] rounded-lg"}>Request a demo</Link>
           </div>
         </div>
       </div>
